a19fba1261689ca91a0ceb9133e4eb00eb571f73
Angular CLI <angular-cli@angular.io>
2017-12-20 09:50:43 +0800

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
+  message: string;
+  ngOnInit(): void {
+    this.http.get('https://api.github.com/users/seeschweiler').subscribe(data => {
+	  message= data;
+      console.log(data);
+    });
+  }
+}
